34fa690b1ee7fbf711c19242605fe4b8c20ea074
nikita nesterov <nikita.nesterov@bas-ip.com>
2023-10-16 18:13:45 +0300

    updated docker startup process, added listen event on off/ topic to turn off sensor

--- main.cpp
+++ main.cpp
@@ -10,16 +10,17 @@
 
 
 std::string generate_uuid_v4();
-const std::string SERVER = "hawk.rmq.cloudamqp.com"; //Broker address
+const std::string SERVER = "hawk.rmq.cloudamqp.com:1883"; //Broker address
 const std::string CLIENT_ID = generate_uuid_v4();
-const std::string TOPIC = "sensors_data";
-const std::string PASSWORD = "hdj973ZX7qrwvZki6_gHAPbP6PZf5eQV";
-const std::string USERNAME = "desizaxr:desizaxr";
+const std::string TOPIC = "sensors_data/" + CLIENT_ID;
+const std::string PASSWORD = "U4p0L3VsKEd_P0uZP57x3k81H3YO3b0V";
+const std::string USERNAME = "pzqsfkxt:pzqsfkxt";
 const int QoS = 1;
 
 std::random_device rd{};
 std::mt19937 gen{rd()};
 std::normal_distribution<float>* distribution;
+float watt_usage;
 
 std::string generate_uuid_v4() {
 	std::uniform_int_distribution<> dis(0, 15);
@@ -55,33 +56,19 @@ std::string generate_uuid_v4() {
 	return ss.str();
 }
 
-std::string form_json(const std::string& name, const std::string& type, float value)
+std::string form_json(const std::string& name, float value)
 {
 	std::string json;
-	json = "{\"name\": \"" + name + "\", \"type\":\"" + type + "\", \"value\":" + std::to_string(value) + "}";
+	json = "{\"name\":\"" + name + "\" , \"value\":" + "\"" + std::to_string(value) + "\", \"uuid\":\"" + CLIENT_ID + "\"}";
 	return json;
 }
 
-void sensor_process(const std::string& name, const std::string& type, mqtt::client& client)
+void sensor_process(const std::string& name, mqtt::client& client)
 {
-	if(type == "smoke")
-	{
-		distribution = new std::normal_distribution<float>{0.5, 0.15};
-	}
-	else if(type == "temp")
-	{
-		distribution = new std::normal_distribution<float>{21.0, 4.0};
-	}
-	else if(type == "oxygen")
-	{
-		distribution = new std::normal_distribution<float>{19.5, 4};
-	}
-	else
-	{
-		std::cerr<<"Sensor of non existing type";
-		exit(2);
-	}
-	std::string payload = form_json(name, type, (*distribution)(gen));
+    distribution = new std::normal_distribution<float>{0, 0.3};
+    watt_usage += (*distribution)(gen);
+
+	std::string payload = form_json(name,  watt_usage);
 	auto pubmsg = mqtt::make_message(TOPIC, payload);
 	pubmsg->set_qos(QoS);
 	client.publish(pubmsg);
@@ -89,29 +76,42 @@ void sensor_process(const std::string& name, const std::string& type, mqtt::clie
 	auto tm = *std::localtime(&t);
 	std::cout << std::put_time(&tm, "%d-%m-%Y %H-%M-%S") << " sent to "<<SERVER<<std::endl;
 	std::cout<<pubmsg.get()->to_string()<<std::endl;
+    bool ok = false;
+    mqtt::const_message_ptr msg;
+    try {
+        ok = client.try_consume_message(&msg);
+    } catch (...) {}
+    if(ok)
+    {
+        if(msg->to_string() == "{\"msg\":\"off\"}")
+        {
+            std::cout << "Turn off message recieved"<<std::endl;
+            exit(0);
+        }
+    }
 	sleep(5);
 }
 
 int main(int argc, char* argv[])
 {
     std::string name = std::getenv("SENSOR_NAME") ? std::getenv("SENSOR_NAME") : "";
-    std::string type = std::getenv("SENSOR_TYPE") ? std::getenv("SENSOR_TYPE") : "";
 
-    if(name.empty() || type.empty())
+    if(name.empty())
     {
-        if(argc != 3)
+        if(argc != 2)
         {
-            std::cerr << "Please pass the name and the type of the sensor in command line arguments";
+            std::cerr << "Please pass the name of the device in as environmental variable";
             exit(1);
         }
 
         name = argv[1];
-        type = argv[2];
     }
+
+    distribution = new std::normal_distribution<float>{120.0, 4};
+    watt_usage = (*distribution)(gen);
 
 	try {
         std::cout<<"sensor_name: "<<name<<std::endl;
-        std::cout<<"sensor_type: "<<type<<std::endl;
         std::cout<<"uuid: "<<CLIENT_ID<<std::endl;
 		mqtt::client client(SERVER, CLIENT_ID);
 		mqtt::connect_options connOpts;
@@ -121,9 +121,10 @@ int main(int argc, char* argv[])
 		connOpts.set_user_name(USERNAME);
 
 		client.connect(connOpts);
+        client.subscribe("off/" + CLIENT_ID, 0);
 
 		while (client.is_connected()) {
-			sensor_process(name, type, client);
+			sensor_process(name, client);
 		}
 	}
 	catch (const mqtt::exception& e)
